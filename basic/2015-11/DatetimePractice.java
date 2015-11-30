50012e05ead2e8a25a5c8a631800de76c9513f13
wujunwei <1399952803@qq.com>
2015-11-30 23:17:00 +0800

    java datetiem practice

--- basic/2015-11/DatetimePractice.java
+++ basic/2015-11/DatetimePractice.java
@@ -1,7 +1,26 @@
 
 // Datetime Practice
+
+import java.util.*;
+import java.text.*;
 class DatetimePractice{
 	public static void main(String[] args) {
-		
+		//实例化时间对象
+		Date d = new Date();
+		System.out.println(d);
+
+		//打印标准时间 2015-11-30 11:11:11
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String time = sdf.format(d);
+		System.out.println(time);
+
+		//获取当前时间戳
+		long l = System.currentTimeMillis();
+		System.out.println(l);
+
+		//通过时间戳实例化时间对象
+		Date d1 = new Date(l);
+		System.out.println(d1);
+
 	}
 }
