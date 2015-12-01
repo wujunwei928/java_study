e0b63bdb00b430c17d31d4f0e2efb4c1d2457c54
wujunwei <1399952803@qq.com>
2015-12-01 23:33:06 +0800

    reg practice

--- basic/2015-11/StringPractice.java
+++ basic/2015-11/StringPractice.java
@@ -1,9 +1,27 @@
 
 // String Practic
+
+import java.util.regex.*;	//正则表达式
 class StringPractice{
 	public static void main(String[] args) {
 		String name = "wujunwei";
 		System.out.println(name.length());
 		System.out.println(name.charAt(6));
+
+		//正则表达式
+		String str = "ming tian jiu yao fang jia le , da jia.";
+		String reg = "\\b[a-z]{4}\\b";
+		Pattern p = Pattern.compile(reg);
+		Matcher m = p.matcher(str);
+		//判断是否匹配到
+		boolean b = m.find();
+		System.out.println(b);
+
+		System.out.println(m.group());
+
+		while (m.find()) {
+			System.out.println(m.group());
+			System.out.println(m.start()+"......"+m.end());
+		}
 	}
 }
